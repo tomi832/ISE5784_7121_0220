bf170bf16a8709a1ba6f20024ce834744b8764e3
YosefKornfeldClion <yosefk7@gmail.com>
2024-07-08 23:55:08 +0300

    galaxy work started

--- unitTests/renderer/BallsTest.java
+++ unitTests/renderer/BallsTest.java
@@ -1,19 +1,10 @@
 package renderer;
 
-import static java.awt.Color.*;
-
-import geometries.Plane;
-import geometries.Polygon;
-import lighting.DirectionalLight;
-import lighting.PointLight;
+import geometries.*;
+import lighting.SpotLight;
 import org.junit.jupiter.api.Test;
 
-import geometries.Sphere;
-import geometries.Triangle;
-import lighting.AmbientLight;
-import lighting.SpotLight;
 import primitives.*;
-import renderer.*;
 import scene.Scene;
 
 public class BallsTest {
@@ -21,41 +12,129 @@ public class BallsTest {
     private final Scene scene = new Scene("Test scene");
 
     private final Camera.Builder cameraBuilder = Camera.getBuilder()
-            .setLocation(new Point(100, 4, 3)).setVpDistance(1000)
+            .setLocation(new Point(100, 20, 3))
+            .setVpDistance(900)
             .setDirection(new Point(0, 5, 3), Vector.Y)
             .setRayTracer(new SimpleRayTracer(scene));
 
+    private final Camera.Builder cameraBuilder2 = Camera.getBuilder()
+            .setLocation(new Point(20, 30, 5))
+            .setVpDistance(100)
+            .setDirection(new Point(0, 5, 3), Vector.Y)
+            .setRayTracer(new SimpleRayTracer(scene));
+
+    public static Double3 tilt(double x, double y, double z, double angle) {
+        double cosA = Math.cos(angle);
+        double sinA = Math.sin(angle);
+        double yNew = y * cosA - z * sinA;
+        double zNew = y * sinA + z * cosA;
+        return new Double3(x, yNew, zNew);
+    }
+
     @Test
-    public void BallsTest1() {
-        Material ballsMaterial = new Material().setKd(0.2).setKs(0.5).setShininess(400).setKr(0.3);
-        Material wallsMaterial = new Material().setKd(0.35).setKs(0.4).setShininess(10);
+    public void GalaxyTest() {
+        Material ballsMaterial = new Material().setKd(0.1);
+        Material wallsMaterial = new Material().setKd(0.35).setKs(0.4).setKt(0.85).setKr(0.2).setShininess(10);
+        Color wallColor = new Color(20, 25, 25); //glass
+        Color midGalaxy = new Color(255, 110, 70); //orange
+        Color sunColor1 = new Color(255, 160, 90); //light orange
+        Color sunColor2 = new Color(255, 200, 50); //yellow
+        Color starColor1 = new Color(80, 255, 230); //light-blue
+        Color starColor2 = new Color (200, 250, 255); //almost-white blue
+        Color dustColor1 = new Color(40, 20, 0); //dark brown
+        Color dustColor2 = new Color(45, 10, 0); //reddish brown
+        Color oldStarsColor = new Color(200, 40, 0); //deep red
         scene.geometries.add(
                 new Plane(new Point(0, 0, 0), new Vector(0, 1, 0))
-                        .setEmission(new Color(50, 50, 50))
-                        .setMaterial(new Material().setKd(1).setShininess(30)),
-                new Sphere(new Point(1, 2.5, 5), 2.5)
-                        .setEmission(new Color(0, 100, 255))
-                        .setMaterial(ballsMaterial),
-                new Sphere(new Point(0, 1.5, 1), 1.5)
-                        .setEmission(new Color(170, 30, 210))
-                        .setMaterial(ballsMaterial),
-                new Triangle(new Point(-30, 0, -30), new Point(-30, 0, 30), new Point(0, 20, 0))
-                        .setEmission(new Color(30,170,60))
+                        .setEmission(new Color(120, 100, 40))
+                        .setMaterial(new Material().setKd(1)),
+                new Polygon(new Point(-1,0,-1),
+                        new Point(-1,0,11),
+                        new Point(-1,11,11),
+                        new Point(-1,11,-1))
+                        .setEmission(wallColor)
                         .setMaterial(wallsMaterial),
-                new Triangle(new Point(-30, 0, 30), new Point(30, 0, 30), new Point(0, 20, 0))
-                        .setEmission(new Color(80,80,80))
+                new Polygon(new Point(-1,11, -1),
+                        new Point(22,11,-1),
+                        new Point(22,0,-1),
+                        new Point(-1,0,-1))
+                        .setEmission(wallColor)
                         .setMaterial(wallsMaterial),
-//                new Triangle(new Point(50, 0, 50), new Point(50, 0, -50), new Point(0, 20, 0))
-//                        .setEmission(new Color(255, 255, 255))
-//                        .setMaterial(wallsMaterial),
-                new Triangle(new Point(30, 0, -30), new Point(-30, 0, -30), new Point(0, 20, 0))
-                        .setEmission(new Color(195,100,30))
+                new Polygon(new Point(22,0,-1),
+                        new Point(22,0,11),
+                        new Point(22,11,11),
+                        new Point(22,11,-1))
+                        .setEmission(wallColor)
+                        .setMaterial(wallsMaterial),
+                new Polygon(new Point(-1,0,11),
+                        new Point(22,0,11),
+                        new Point(22,11,11),
+                        new Point(-1,11,11))
+                        .setEmission(wallColor)
+                        .setMaterial(wallsMaterial),
+                new Polygon(new Point(-1, 11, -1),
+                        new Point(-1, 11, 11),
+                        new Point(22, 11, 11),
+                        new Point(22, 11, -1))
+                        .setEmission(wallColor)
                         .setMaterial(wallsMaterial)
+
         );
-        scene.lights.add(new PointLight(new Color(255, 255, 255), new Point(10, 15, -9)).setKl(0.001).setKq(0.0001));
+
+        /** creating the arms of the galaxy */
+        double a = 2d; //Distance between spiral arms
+        double b = 0.25; //tightness of the spiral
+        double r;
+        double x;
+        double z;
+        double y;
+        int numOfSpheres = 50;
+        double angleIncrement = 0.15;
+        int numArms = 10; //number of arms for the galaxy
+        double theta;
+        double angleOffset;
+        for (int arm = 0; arm < numArms; arm++) {
+            angleOffset = (2 * Math.PI / numArms) * arm;
+            for (int i = 0; i < numOfSpheres; i++) {
+                theta = i * angleIncrement;
+                r = a * Math.exp(b * theta);
+                x = r * Math.cos(theta + angleOffset);
+                z = r * Math.sin(theta + angleOffset);
+                y = Math.random() * 0.2 - 0.1;
+                if (arm == 0)
+                    scene.geometries.add(new Sphere(new Point(x + 4, y + 4, z + 4), 0.2)
+                        .setEmission(sunColor2)
+                        .setMaterial(ballsMaterial));
+                else if (arm == 1) {
+                    scene.geometries.add(new Sphere(new Point(x + 4, y + 4, z + 4), 0.3)
+                            .setEmission(sunColor1)
+                            .setMaterial(ballsMaterial));
+                }  else if (arm % 2 == 0){
+                    scene.geometries.add(new Sphere(new Point(x + 4, y + 4, z + 4), 0.1)
+                            .setEmission(starColor1)
+                            .setMaterial(ballsMaterial));
+                }
+                else {
+                    scene.geometries.add(new Sphere(new Point(x + 4, y + 4, z + 4), 0.1)
+                            .setEmission(starColor2)
+                            .setMaterial(ballsMaterial));
+                }
+            }
+        }
+        scene.geometries.add(new Sphere(new Point(4, 4, 4), 2)
+                .setEmission(midGalaxy)
+                .setMaterial(ballsMaterial));
+
+        //scene.lights.add(new SpotLight())
 
         cameraBuilder.setVpSize(250, 250)
-                .setImageWriter(new ImageWriter("Balls.txt", 1200, 1200))
+                .setImageWriter(new ImageWriter("Balls.txt", 600, 600))
+                .build()
+                .renderImage()
+                .writeToImage();
+
+        cameraBuilder2.setVpSize(250,250)
+                .setImageWriter(new ImageWriter("Balls2.txt", 1200, 1200))
                 .build()
                 .renderImage()
                 .writeToImage();
@@ -63,26 +142,3 @@ public class BallsTest {
 }
 
 
-//                new Polygon(
-//                        new Point(-2, 0, -2),
-//                        new Point(2, 0, -2),
-//                        new Point(2, 10, -2),
-//                        new Point(-2, 10, -2))
-//                        .setEmission(new Color(30,170,60))
-//                        .setMaterial(wallsMaterial),
-//
-//                new Polygon(
-//                        new Point(-2, 0, -2),
-//                        new Point(-2,0,9),
-//                        new Point(-2, 10, 9),
-//                        new Point(-2, 10, -2))
-//                        .setEmission(new Color(80,80,80))
-//                        .setMaterial(wallsMaterial),
-//
-//                new Polygon(
-//                        new Point(-2, 0, 9),
-//                        new Point(20, 0, 9),
-//                        new Point(20, 10, 9),
-//                        new Point(-2, 10, 9))
-//                        .setEmission(new Color(195,100,30))
-//                        .setMaterial(wallsMaterial)
