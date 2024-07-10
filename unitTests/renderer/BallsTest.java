de6c940ffc33eebb6c28dd172b35204a05197904
YosefKornfeldClion <yosefk7@gmail.com>
2024-07-10 22:24:57 +0300

    UNUSABLE VERSION

--- unitTests/renderer/BallsTest.java
+++ unitTests/renderer/BallsTest.java
@@ -28,12 +28,15 @@ public class BallsTest {
         return new double[]{x + 4, yNew + 8, zNew + 4};
     }
 
+
     @Test
     public void GalaxyTest() {
         final Material ballsMaterial = new Material().setKd(0.2).setKs(0.33).setShininess(10);
         final Material glassMaterial = new Material().setKd(0.35).setKs(0.4).setKt(0.85).setKr(0.2).setShininess(10);
+        final Material frameMaterial = new Material().setKd(0.5).setKs(0.35).setKr(0.05).setShininess(10);
         final Material pedestalMaterial = new Material().setKd(0.5).setKs(0.35).setShininess(10);
         final Color glassColor = new Color(20, 25, 25); //glass
+        final Color frameColor = new Color(30, 30, 30); //dark grey
         final Color pedestalColor = new Color(80, 40, 5); //brown
         final Color midGalaxy = new Color(255, 220, 110); //light yellow
         final Color sunColor1 = new Color(255, 150, 90); //light orange
@@ -44,27 +47,58 @@ public class BallsTest {
         final Color dustColor2 = new Color(65, 10, 0); //reddish brown
         final Color oldStarsColor = new Color(200, 40, 0); //deep red
 
+        /** geometries for the scene */
+        Geometries galaxy = new Geometries();
+        Geometries pedestal = new Geometries();
+        Geometries box = new Geometries();
+        Geometries frame = new Geometries();
+
         /** coordinates for the walls of the box */
         final double x1 =-6.5d, x2 = 14.5d;
         final double z1 =-6.5d, z2 = 14.5d;
         final double y1 = 4d, y2 = 15d;
 
-        /** coordinates for the top level of the pedestal */
-        final double x3 = -7, x4 = 15;
-        final double z3 = -7, z4 = 15;
-        final double y3 = 2d, y4 = 4d;
+        /** coordinates for the frame of the box */
+        final double FRAME_WIDTH = 0.15;
+        final double x11 = x1 - FRAME_WIDTH, x12 = x1 + FRAME_WIDTH;
+        final double z11 = z1 - FRAME_WIDTH, z12 = z1 + FRAME_WIDTH;
+        final double y11 = y1 - FRAME_WIDTH, y12 = y1 + FRAME_WIDTH;
+        final double x21 = x2 - FRAME_WIDTH, x22 = x2 + FRAME_WIDTH;
+        final double z21 = z2 - FRAME_WIDTH, z22 = z2 + FRAME_WIDTH;
+        final double y21 = y2 - FRAME_WIDTH, y22 = y2 + FRAME_WIDTH;
 
-        /** coordinates for the bottom level of the pedestal */
-        final double x5 = -7.5, x6 = 15.5;
-        final double z5 = -7.5, z6 = 15.5;
-        final double y5 = 0, y6 = 2;
-        scene.geometries.add(
-                new Plane(new Point(0, 0, 0), new Vector(0, 1, 0))
-                        .setEmission(new Color(30, 40, 70))
-                        .setMaterial(new Material().setKd(1)),
+        frame.add(new Polygon(
+                             new Point(x11,y11,z12),
+                             new Point(x12,y11,z12),
+                             new Point(x12,y12,z12),
+                             new Point(x11,y12,z12))
+                             .setEmission(frameColor)
+                             .setMaterial(frameMaterial),
+                 new Polygon(
+                             new Point(x11,y11,z11),
+                             new Point(x11,y11,z12),
+                             new Point(x11,y12,z12),
+                             new Point(x11,y12,z11))
+                            .setEmission(frameColor)
+                            .setMaterial(frameMaterial),
+                 new Polygon(
+                             new Point(x11,y12,z11),
+                             new Point(x12,y12,z11),
+                             new Point(x12,y12,z12),
+                             new Point(x11,y12,z12))
+                            .setEmission(frameColor)
+                            .setMaterial(frameMaterial),
+                 new Polygon(
+                            new Point(x12,,),
+                            new Point(x12,,),
+                            new Point(x12,,),
+                            new Point(x12,,)
+                            .setEmission(frameColor)
+                            .setMaterial(frameMaterial)
+                 )
+        );
 
-                //Glass box (with regards for galaxy.txt)
-                new Polygon(
+        box.add(new Polygon(
                         new Point(x1,y1,z2),
                         new Point(x2,y1,z2),
                         new Point(x2,y2,z2),
@@ -91,18 +125,20 @@ public class BallsTest {
                         new Point(x2,y2,z2),
                         new Point(x2,y2,z1))
                         .setEmission(glassColor)
-                        .setMaterial(glassMaterial),
-                //Ceiling
-                new Polygon(
-                        new Point(x1,y2,z1),
-                        new Point(x1,y2,z2),
-                        new Point(x2,y2,z2),
-                        new Point(x2,y2,z1))
-                        .setEmission(glassColor)
-                        .setMaterial(glassMaterial),
+                        .setMaterial(glassMaterial)
+        );
 
-                //top level of the pedestal
-                new Polygon(
+        /** coordinates for the top level of the pedestal */
+        final double x3 = -7, x4 = 15;
+        final double z3 = -7, z4 = 15;
+        final double y3 = 2d, y4 = 4d;
+
+        /** coordinates for the bottom level of the pedestal */
+        final double x5 = -7.5, x6 = 15.5;
+        final double z5 = -7.5, z6 = 15.5;
+        final double y5 = 0, y6 = 2;
+
+        pedestal.add( new Polygon(
                         new Point(x3,y3,z4),
                         new Point(x4,y3,z4),
                         new Point(x4,y4,z4),
@@ -173,8 +209,10 @@ public class BallsTest {
                         new Point(x6,y6,z6),
                         new Point(x6,y6,z5))
                         .setEmission(pedestalColor)
-                        .setMaterial(pedestalMaterial)
-        );
+                        .setMaterial(pedestalMaterial));
+
+
+
 
         /** creating the arms of the galaxy */
         final double a = 1.7; //Distance between spiral arms
@@ -201,35 +239,42 @@ public class BallsTest {
                     if (rand < 0.1) {
                     }
                     else if (rand < 0.5) {
-                        scene.geometries.add(new Sphere(new Point(tiltedParams[0], tiltedParams[1], tiltedParams[2]), 0.2 + Math.random() % 0.03)
+                        galaxy.add(new Sphere(new Point(tiltedParams[0], tiltedParams[1], tiltedParams[2]), 0.2 + Math.random() % 0.03)
                                 .setEmission(sunColor1.scale(0.9 + (rand % 0.2)))
                                 .setMaterial(ballsMaterial));
                     } else {
-                        scene.geometries.add(new Sphere(new Point(tiltedParams[0], tiltedParams[1], tiltedParams[2]), 0.16+ Math.random() % 0.03)
+                        galaxy.add(new Sphere(new Point(tiltedParams[0], tiltedParams[1], tiltedParams[2]), 0.16+ Math.random() % 0.03)
                                 .setEmission(sunColor2.scale(0.9 + (rand % 0.2)))
                                 .setMaterial(ballsMaterial));
                     }
                 }
                 else {
                     if (Math.random() < 0.5)
-                        scene.geometries.add(new Sphere(new Point(tiltedParams[0], tiltedParams[1], tiltedParams[2]), 0.05)
+                        galaxy.add(new Sphere(new Point(tiltedParams[0], tiltedParams[1], tiltedParams[2]), 0.05)
                             .setEmission(starColor1)
                             .setMaterial(ballsMaterial));
                     else
-                        scene.geometries.add(new Sphere(new Point(tiltedParams[0], tiltedParams[1], tiltedParams[2]), 0.05)
+                        galaxy.add(new Sphere(new Point(tiltedParams[0], tiltedParams[1], tiltedParams[2]), 0.05)
                                 .setEmission(starColor2)
                                 .setMaterial(ballsMaterial));
                 }
             }
         }
 
-        scene.geometries.add(
+        galaxy.add(
                 new Sphere(new Point(4, 8, 4), 1)
                 .setEmission(midGalaxy)
                 .setMaterial(ballsMaterial),
                 new Sphere(new Point(4, 8, 4), 1.3)
                 .setEmission(midGalaxy.scale(0.3))
                 .setMaterial(new Material().setKt(0.9).setKd(0.1).setKs(0.9).setShininess(10))
+        );
+
+        scene.geometries.add(
+                new Plane(new Point(0, 0, 0), new Vector(0, 1, 0))
+                        .setEmission(new Color(30, 40, 70))
+                        .setMaterial(new Material().setKd(1)),
+                galaxy, pedestal, box, frame
         );
 
         scene.lights.add(
